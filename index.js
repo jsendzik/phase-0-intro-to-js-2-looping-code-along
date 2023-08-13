99c0aa541ffc3e8f8ca737615c1fd34912e56727
Jacob Sendzik <jake.sendzik@gmail.com>
2023-08-13 18:42:00 -0400

    complete?

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const names = ["Jacob", "Keely", "Joe"];
+const card = [];
+function writeCards(name, event) {
+    for (let i = 0; i < 3; i++) {
+        card.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+        console.log(card)
+    }
+    return card
+}
+
+
+function countDown(num) {
+    let i = num;
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
